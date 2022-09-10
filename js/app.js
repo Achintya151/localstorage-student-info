abfe835949a59be3d4c3e9bf3d772fb6b330b247
Achintya151 <achintyakumartalukdar@gmail.com>
2022-09-10 15:04:19 +0600

    dom element added

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// DOM Element
+const studentForm = document.getElementById('studentForm');
+const studentsContainer = document.querySelector('.students');
+const nameInput = studentForm['name'];
+const ageInput = studentForm['age'];
+const rollInput = studentForm['roll'];
