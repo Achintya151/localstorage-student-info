c6cb1117796feffdb6b79d31f42ffbd45404a74f
Achintya151 <achintyakumartalukdar@gmail.com>
2022-09-10 15:16:56 +0600

    create element function added

--- js/app.js
+++ js/app.js
@@ -4,3 +4,29 @@ const studentsContainer = document.querySelector('.students');
 const nameInput = studentForm['name'];
 const ageInput = studentForm['age'];
 const rollInput = studentForm['roll'];
+
+const students = [
+    {
+        name: '',
+        age: Number,
+        roll: Number
+    }
+];
+
+const addStudents = (name, age, roll) => {
+
+}
+
+const createStudentElement = ({ name, age, roll }) => {
+    const studentDiv = document.createElement('div');
+    const studentName = document.createElement('h2');
+    const studentAge = document.createElement('p');
+    const studentRoll = document.createElement('p');
+
+    studentName.innerText = "Student Name:" + name;
+    studentAge.innerText = "Student Age -" + age;
+    studentRoll.innerText = "Student Roll -" + roll;
+
+    studentDiv.append(studentName, studentAge, studentRoll);
+    studentsContainer.appendChild(studentDiv);
+}
